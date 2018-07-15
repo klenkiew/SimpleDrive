4f4f7f9b7293b88f3a8562f639194e93941e8b4e
klenkiew <k.lenkiewicz@protonmail.com>
2018-07-15 14:31:41 +0200

    Add basic logging to RedisEventBus

--- Libraries/Redis/RedisEventBus.cs
+++ Libraries/Redis/RedisEventBus.cs
@@ -1,24 +1,41 @@
-﻿using EventBus;
+﻿using System;
+using EventBus;
+using Microsoft.Extensions.Logging;
 
 namespace Redis
 {
     public class RedisEventBus : ITypedEventBus<string>
     {
         private readonly IRedisConnectionFactory connectionFactory;
-        
-        public RedisEventBus(IRedisConnectionFactory connectionFactory)
+        private readonly ILogger<RedisEventBus> logger;
+
+        public RedisEventBus(IRedisConnectionFactory connectionFactory, ILoggerFactory loggerFactory)
         {
             this.connectionFactory = connectionFactory;
+            this.logger = loggerFactory.CreateLogger<RedisEventBus>();
         }
 
         public void Subscribe(string topic, IMessageHandler<string> messageHandler)
         {
+            logger.LogDebug("[Redis] Subscribing: " + topic);
             var subscriber = connectionFactory.Connection.GetSubscriber();
-            subscriber.Subscribe(topic, (channel, message) => messageHandler.Handle(message));
+            subscriber.Subscribe(topic, (channel, message) =>
+            {
+                try
+                {
+                    logger.LogTrace("[Redis] Handler for: " + channel);
+                    messageHandler.Handle(message);
+                }
+                catch (Exception ex)
+                {
+                    logger.LogError(ex, $"[Redis] Unhandled exception in message handler for topic {topic}");
+                }
+            });
         }
 
         public void Publish(string topic, string message)
         {
+            logger.LogDebug("[Redis] Publishing: " + topic);
             connectionFactory.Connection.GetDatabase().Publish(topic, message);
         }
     }
