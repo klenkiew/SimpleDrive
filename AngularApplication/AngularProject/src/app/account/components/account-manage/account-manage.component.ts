1acf3a2a34bc5da305953e306e5ab8c0d870c492
klenkiew <k.lenkiewicz@protonmail.com>
2018-07-18 00:49:16 +0200

    Make the change email and password fieldsets expanded by default on large screens

--- AngularApplication/AngularProject/src/app/account/components/account-manage/account-manage.component.ts
+++ AngularApplication/AngularProject/src/app/account/components/account-manage/account-manage.component.ts
@@ -7,9 +7,13 @@ import {Component, OnInit} from "@angular/core";
 })
 export class AccountManageComponent implements OnInit {
 
+  collapsed: boolean = true;
+
   constructor() {
   }
 
   ngOnInit() {
+    if (window.innerWidth > 992)
+      this.collapsed = false;
   }
 }
