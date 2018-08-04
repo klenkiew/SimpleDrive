7f52b494f173c15b58fd60bde8432198beff52de
klenkiew <k.lenkiewicz@protonmail.com>
2018-08-04 21:34:52 +0200

    Remove an inaccurate comment

--- FileService.Tests/Helpers/EntityHelper.cs
+++ FileService.Tests/Helpers/EntityHelper.cs
@@ -7,7 +7,6 @@ namespace FileService.Tests.Helpers
     {
         public static void SetId<T>(T entity, string id) where T : IEntity
         {
-            // set the id property on file to prevent bugs due to null id
             PropertyInfo prop = entity.GetType().GetProperty("Id", BindingFlags.NonPublic | BindingFlags.Instance 
                                                                                           | BindingFlags.Public);
             prop.SetValue(entity, id);
